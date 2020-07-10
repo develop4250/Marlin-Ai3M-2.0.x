8a6c8d687d384920f7cb941a2519e4b4ec89d568
Mark <mwarner4250@gmail.com>
2020-07-10 13:35:50 +0930

    Temperature update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1490,13 +1490,13 @@
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 200
-#define PREHEAT_1_TEMP_BED     50
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_1_TEMP_BED     60
+#define PREHEAT_1_FAN_SPEED     180 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED     80
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 235
+#define PREHEAT_2_TEMP_BED     70
+#define PREHEAT_2_FAN_SPEED     75 // Value from 0 to 255
 
 /**
  * Nozzle Park
